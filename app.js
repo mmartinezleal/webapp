45eaaec065c1843fe95d01d6e38da03fc2945db9
mmartinezleal <mmartinezleal@deloitte.com>
2025-04-03 15:52:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 const port = process.env.PORT | 3000;
 
-app.get("/", async (req,res)=>{
+app.get("/private", async (req,res)=>{
     try{
         const url = "http://mma-funcapp.azurewebsites.net";
         const response = await.axios.get(url);
